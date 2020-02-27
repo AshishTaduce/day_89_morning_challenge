e09640c28895d8cac195d71cfd642978547e5a45
AshishTaduce <ashishteddy2@gmail.com>
2020-02-27 11:01:43 +0530

    logic done, writing test cases

--- bin/main.dart
+++ bin/main.dart
@@ -2,33 +2,50 @@
 // The output should be a sorted list as well.
 
 void main() {
-
+List a = [1,3,4,5,7,];
+List b = [0,2,4,6,8];
+print(mergeList(a, b));
  print('HEY');
 }
 //Ascending Order
 List mergeList(List list1, List list2){
- int listLength = list1.length < list2.length
-     ? list1.length : list2.length;
+ int listLength;
  List small = list1.length < list2.length
      ? list1 : list2;
  List big = list1.length > list2.length
      ? list1 : list2;
- List result;
+ if(list1.length < list2.length){
+  small = list1;
+  big = list2;
+ }
+ else if(list1.length > list2.length){
+  small = list2;
+  big = list1;
+ }
+ else{
+  small = list1;
+  big = list2;
+ }
+
+ listLength = small.length;
+ List result = [];
  for(int i = 0; i < listLength; i++){
-  if(small[i] < big[i]){
-   result.addAll([small[i], big[i]]);
-   big.removeAt(i);
+  print(i);
+  print(small[i]);
+  if(small[i] < big[0]){
+   result.addAll([small[i], big[0]]);
+   big.removeAt(0);
   }
-  else if(small[i] > big[i]){
-   result.addAll([big[i], small[i]]);
-   big.removeAt(i);
+  else if(small[i] > big[0]){
+   result.addAll([big[0], small[i]]);
+   big.removeAt(0);
   }
   else {
-   result.addAll([big[i], small[i]]);
-   big.removeAt(i);
+   result.addAll([big[0], small[i]]);
+   big.removeAt(0);
   }
  }
- result.addAll(big);
+ if(big.isNotEmpty)result.addAll(big);
  return result;
 }
 
