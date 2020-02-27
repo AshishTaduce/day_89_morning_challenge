90a1de2e445dddd6c84a129bbedc6f9b9ae9afd5
AshishTaduce <ashishteddy2@gmail.com>
2020-02-27 10:44:28 +0530

    a

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,26 @@
 // The output should be a sorted list as well.
 
 void main() {
+
  print('HEY');
+}
+//Ascending Order
+List mergeList(List list1, List list2){
+ int listLength = list1.length < list2.length
+     ? list1.length : list2.length;
+ List small = list1.length < list2.length
+     ? list1 : list2;
+ List big = list1.length > list2.length
+     ? list1 : list2;
+ List result;
+ for(int i = 0; i < listLength; i++){
+  if(small[i] < big[i]){
+   result.addAll([small[i], big[i]]);
+  }
+  if(small[i] > big[i]){
+   result.addAll([big[i], small[i]]);
+  }
+ }
+ return result;
 }
 
